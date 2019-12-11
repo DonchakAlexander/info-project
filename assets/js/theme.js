19cc9f3be7e5f515468e701c2d54bcdb95395328
DonchakAlexander <dag@webernetic.by>
2019-12-11 12:54:45 +0300

    Some bugs fixed

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -393,11 +393,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         _self.find('.sub-menu').fadeOut(800);
     });
 
-    $('div.elementor-tabs-wrapper').on('click', 'div:not(.elementor-active)', function() {
-        $(this)
-            .addClass('elementor-active').siblings().removeClass('elementor-active')
-            .closest('div.elementor-tabs').find('div.elementor-tab-content').css('display', 'none').removeClass('elementor-active').eq($(this).index()).css('display', 'block').addClass('elementor-active');
-    });
+
 
 
     $(".owl-carousel").on("initialized.owl.carousel", () => {
@@ -419,36 +415,14 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
         const $currentOwlItem = $(".owl-item").eq(e.item.index);
         $currentOwlItem.find(".owl-slide-animated").addClass("is-transitioned");
-
-        const $target = $currentOwlItem.find(".owl-slide-text");
-        doDotsCalculations($target);
     });
 
-    $owlCarousel.on("resize.owl.carousel", () => {
-        setTimeout(() => {
-            setOwlDotsPosition();
-        }, 50);
+
+    $('div.elementor-tabs-wrapper').on('click', 'div:not(.elementor-active)', function() {
+        $(this)
+            .addClass('elementor-active').siblings().removeClass('elementor-active')
+            .closest('div.elementor-tabs').find('div.elementor-tab-content').css('display', 'none').removeClass('elementor-active').eq($(this).index()).css('display', 'block').addClass('elementor-active');
     });
-
-    setOwlDotsPosition();
-
-    function setOwlDotsPosition() {
-        const $target = $(".owl-item.active .owl-slide-text");
-        doDotsCalculations($target);
-    }
-
-    function doDotsCalculations(el) {
-        const height = el.height();
-        const {top, left} = el.position();
-        const res = height + top + 20;
-
-        $(".owl-carousel .owl-dots").css({
-            top: `${res}px`,
-            left: `${left}px`
-        });
-    }
-
-
 
 
     jQuery(document).ready(function ($) {
